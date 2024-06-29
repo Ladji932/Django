0ceb127ac9bd123512754af496ee93ef12e8b930
Ladji <ladjimaiga@haouas-Air.lan>
2024-06-29 19:05:39 +0200

    config deploiement

--- multilang_site/settings.py
+++ multilang_site/settings.py
@@ -16,7 +16,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 # Application definition
