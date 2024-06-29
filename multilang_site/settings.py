8a8226f71bf7d6e33a14f7c824b1b612bb86da04
Ladji <ladjimaiga@haouas-Air.lan>
2024-06-29 18:42:30 +0200

    config deploiement

--- multilang_site/settings.py
+++ multilang_site/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['django-multilang.onrender.com','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
